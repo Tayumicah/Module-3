833e74a4c4119fe27dca56494ef95a78fa807451
Tayumicah <74253156+Tayumicah@users.noreply.github.com>
2022-07-27 22:30:53 -0400

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,3 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,7 +6,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+function generatePassword() {
+  var uppCaseupp = "ABCDEFGHIJKLOMNOPQRSTUVWXYZ";
+  var lowerCaselow = "abcdefghijklomnopqrstuvwxyz";
+  var specialChar = "!@#$%^&*";
+  var number = "0123456789";
+  var allChar = uppCaseupp + lowerCaselow + specialChar + number;
+  var password = "";
 
+  var uppIndex = Math.floor(Math.random() * uppCaseupp.length);
+  password += uppCaseupp.charAt(uppIndex);
+
+  var lowIndex = Math.floor(Math.random() * lowerCaselow.length);
+  password += lowerCaselow.charAt(lowIndex);
+
+  var speciIndex = Math.floor(Math.random() * specialChar.length);
+  password += specialChar.charAt(speciIndex);
+
+  var numberIndex = Math.floor(Math.random() * number.length);
+  password += number.charAt(numberIndex);
+
+  for (let i = 1; i <= 4; i++) {
+    var allIndex = Math.floor(Math.random() * allChar.length);
+    password += allChar.charAt(allIndex);
+  }
+  return password;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password Generator Starter Code
+
+Description password generator with four variables then a for loop which creates the next 4 characters for the password generator.
+
+Screenshot
+![screenshot](/Users/tayusama/Documents/GitHub/friendly-parakeet/assets/Screen Shot 2022-07-27 at 7.51.42 PM (2).png)
